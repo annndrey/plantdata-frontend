26680c488b16b97043161c81fe52847bbbf32894
annndrey <annndrey@trololo.info>
2019-09-12 13:01:54 +0300

    Added weight data display to the site

--- client/src/components/MainPage.vue
+++ client/src/components/MainPage.vue
@@ -52,6 +52,7 @@ export default {
 	    temp1: null,
 	    tempA: null,
 	    lux: null,
+	    whgt0: null,
 	    plotdata: null,
 	    labels: [],
 	    dataset: [],
@@ -133,6 +134,7 @@ export default {
 		this.temp1 = []
 		this.tempA = []
 		this.co2 = []
+		this.wght0 = []
 		this.imgcount = 0
 		this.pictindex = 0
 		this.sensordata.map(obj => {
@@ -146,6 +148,7 @@ export default {
 		    this.temp1.push(obj.temp1)
 		    this.tempA.push(obj.tempA)
 		    this.co2.push(obj.co2)
+		    this.wght0.push(obj.wght0)
 		    let pictlist = []
 		    obj.pictures.map( p => {
 			pictlist.push([obj.ts,  p.fpath])
@@ -296,6 +299,27 @@ export default {
 				    data: this.co2,
 				    spanGaps: false
 				   })
+		this.datasets.push({label: "WGHT0",
+				    lineTension: 0.1,
+				    backgroundColor: "rgba(181, 34, 226, 0.2)",
+				    borderColor: "rgba(181, 34, 226,1)",
+				    borderCapStyle: 'butt',
+				    borderDash: [],
+				    borderDashOffset: 0.0,
+				    borderJoinStyle: 'miter',
+				    pointBorderColor: "181, 34, 226,1)",
+				    pointBackgroundColor: "#fff",
+				    pointBorderWidth: 1,
+				    pointHoverRadius: 5,
+				    pointHoverBackgroundColor: "rgba(181, 34, 226,1)",
+				    pointHoverBorderColor: "rgba(220,220,220,1)",
+				    pointHoverBorderWidth: 2,
+				    pointRadius: 1,
+				    pointHitRadius: 10,
+				    data: this.wght0,
+				    spanGaps: false
+				   })
+
 		
 	    }
 	    
