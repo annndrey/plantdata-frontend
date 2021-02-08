244925e0163823ce13f18939a2622c27fa2922d3
Andrew <gontchar@gmail.com>
2021-02-08 23:45:29 +0300

    fix

--- client/src/main.js
+++ client/src/main.js
@@ -23,7 +23,7 @@ Vue.config.productionTip = false
 // Setting up Axios on Vue Instance, for use via this.$axios
 Vue.prototype.$axios = axios.create(axiosConfig);
 Vue.prototype.$axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('token')}`;
-Vue.prototype.$backendhost = 'https://dev.plantdata.fermata.tech:5598/api/v2/'
+Vue.prototype.$backendhost = 'https://host:port/api/v2/'
 Vue.prototype._ = _
 const moment = require('moment')
 require('moment/locale/ru')
